49d8921a6825d76a7c046d3995418137505756b2
Beggy2000 <beggycode@gmail.com>
2019-03-24 12:22:35 +0000

    create ovpn file by default

--- script/createClient.sh
+++ script/createClient.sh
@@ -17,30 +17,21 @@
 #      REVISION:  ---
 #===============================================================================
 
-if [[ -z "$1" ]]; then 
-    echo "Usage: $0 <client name>"
-    exit 1
-fi
+#===  FUNCTION  ================================================================
+#          NAME:  createFullArchive
+#   DESCRIPTION:  
+#    PARAMETERS:
+#       RETURNS:
+#===============================================================================
+function createFullArchive () {
 
-declare -r CLIENT_NAME="$1"
-source ./environment.sh
+    declare -r RELATIVE_CLIENT_CONF="${CLIENT_CONF#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_CLIENT_CRT="${CLIENT_CRT#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_CLIENT_KEY="${CLIENT_KEY#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_CA_CRT="${CA_CRT#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_TSL_SECRET="${TSL_SECRET#${OPEN_VPN_DIR}/}"
 
-if [[ -z "${SERVER_IP}" ]]; then
-    echo "Please define SERVER_IP in ./environment.sh"
-    exit 1
-fi
-
-cd "${EASY_RSA_DIR}"
-source ./vars;
-./build-key "${CLIENT_NAME}"
-
-declare -r RELATIVE_CLIENT_CONF="${CLIENT_CONF#${OPEN_VPN_DIR}/}"
-declare -r RELATIVE_CLIENT_CRT="${CLIENT_CRT#${OPEN_VPN_DIR}/}"
-declare -r RELATIVE_CLIENT_KEY="${CLIENT_KEY#${OPEN_VPN_DIR}/}"
-declare -r RELATIVE_CA_CRT="${CA_CRT#${OPEN_VPN_DIR}/}"
-declare -r RELATIVE_TSL_SECRET="${TSL_SECRET#${OPEN_VPN_DIR}/}"
-
-cat - > "${CLIENT_CONF}" <<END_OF_CLIENT_CONF
+    cat - > "${CLIENT_CONF}" <<END_OF_CLIENT_CONF
 #client option implies pull as well
 client
 
@@ -90,7 +81,112 @@ down-pre
 
 # prevent DNS leakage (see: https://github.com/systemd/systemd/issues/6076#issuecomment-387332572)
 dhcp-option DOMAIN-ROUTE .
+
 END_OF_CLIENT_CONF
 
-tar --auto-compress --create --directory "${OPEN_VPN_DIR}" --file "${CURRENT_DIR}/${CLIENT_NAME}.tar.gz" "${RELATIVE_CLIENT_CONF}" "${RELATIVE_CLIENT_CRT}" "${RELATIVE_CLIENT_KEY}" "${RELATIVE_CA_CRT}" "${RELATIVE_TSL_SECRET}" 
-rm "${CLIENT_CONF}" 
+    tar --auto-compress --create --directory "${OPEN_VPN_DIR}" --file "${CURRENT_DIR}/${CLIENT_NAME}.tar.gz" "${RELATIVE_CLIENT_CONF}" "${RELATIVE_CLIENT_CRT}" "${RELATIVE_CLIENT_KEY}" "${RELATIVE_CA_CRT}" "${RELATIVE_TSL_SECRET}" 
+    rm "${CLIENT_CONF}" 
+}
+# ----------  end of function createFullArchive  ----------
+
+
+#===  FUNCTION  ================================================================
+#          NAME:  createOVPNArchive
+#   DESCRIPTION:  
+#    PARAMETERS:
+#       RETURNS:
+#===============================================================================
+function createOVPNArchive () {
+    declare -r RELATIVE_CLIENT_OVPN="${CLIENT_OVPN#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_CLIENT_CRT="${CLIENT_CRT#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_CLIENT_KEY="${CLIENT_KEY#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_CA_CRT="${CA_CRT#${OPEN_VPN_DIR}/}"
+    declare -r RELATIVE_TSL_SECRET="${TSL_SECRET#${OPEN_VPN_DIR}/}"
+
+    cat - > "${CLIENT_OVPN}" <<END_OF_CLIENT_OVPN
+#client option implies pull as well
+client
+
+#connect to VPN server
+remote ${SERVER_IP} 1194
+
+dev tun
+proto tcp
+
+#remove to use your ISP's gateway
+redirect-gateway def1
+
+#define default direction
+key-direction 1
+
+#keep trying indefinitely to resolve the host name of the OpenVPN server.
+resolv-retry infinite
+
+#most clients don't need to bind to a specific local port number.
+nobind
+
+#set log file verbosity.
+verb 3
+
+#silence repeating messages
+mute 20
+
+# Enable compression on the VPN link.
+compress lz4-v2
+
+#Encrypt data channel packets with cipher algorithm
+cipher AES-256-CBC
+
+#Authenticate data channel packets and (if enabled) tls-auth control channel packets
+auth SHA256
+
+#Allow calling of built-in executables and user-defined scripts.
+script-security 2
+
+#script update DNS(/etc/resolv.conf)
+up /etc/openvpn/update-systemd-resolved
+down /etc/openvpn/update-systemd-resolved
+down-pre
+
+# prevent DNS leakage (see: https://github.com/systemd/systemd/issues/6076#issuecomment-387332572)
+dhcp-option DOMAIN-ROUTE .
+
+#your access keys
+<key>
+$(cat ${CLIENT_KEY})
+</key>
+<cert>
+$(cat ${CLIENT_CRT})
+</cert>
+<ca>
+$(cat ${CA_CRT})
+</ca>
+<tls-auth>
+$(cat ${TSL_SECRET})
+</tls-auth>
+END_OF_CLIENT_OVPN
+
+    tar --auto-compress --create --directory "${OPEN_VPN_DIR}" --file "${CURRENT_DIR}/${CLIENT_NAME}.tar.gz" "${RELATIVE_CLIENT_OVPN}" 
+    rm "${CLIENT_OVPN}" 
+}
+# ----------  end of function createOVPNArchive  ----------
+
+if [[ -z "$1" ]]; then 
+    echo "Usage: $0 <client name>"
+    exit 1
+fi
+
+declare -r CLIENT_NAME="$1"
+source ./environment.sh
+
+if [[ -z "${SERVER_IP}" ]]; then
+    echo "Please define SERVER_IP in ./environment.sh"
+    exit 1
+fi
+
+cd "${EASY_RSA_DIR}"
+source ./vars;
+./build-key "${CLIENT_NAME}"
+
+#createFullArchive
+createOVPNArchive
